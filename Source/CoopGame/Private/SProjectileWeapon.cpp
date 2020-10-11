5b67eded1daf29742fc7e7fe42a103e0f83af00a
Luke Ryan <github@lukeryan.net.nz>
2020-10-11 20:59:30 +1300

    Feedback from lecture 63, how Tom completed challenge. Null checks and projectile spawns from rotation of actors eyes, not exactly where the gun is.

--- Source/CoopGame/Private/SProjectileWeapon.cpp
+++ Source/CoopGame/Private/SProjectileWeapon.cpp
@@ -5,13 +5,24 @@
 
 void ASProjectileWeapon::Fire()
 {
+    AActor* MyOwner = GetOwner();
+    if(!ProjectileClass || !MyOwner)
+    {
+        return;
+    }
+
     //project projectile actor who explodes and causes damage to all around after 2 seconds
     FVector MuzzleLocation = MeshComponent->GetSocketLocation(MuzzleSocketName);
     FRotator MuzzleRotation = MeshComponent->GetSocketRotation(MuzzleSocketName);
 
+    FVector EyeLocation;
+    FRotator EyeRotation;
+
+    MyOwner->GetActorEyesViewPoint(EyeLocation, EyeRotation);
+
     // spawn the projectile at the muzzle.
     // deferred spawn allows us to set props on the object before it is spawned.
-    ASProjectile* projectile = GetWorld()->SpawnActorDeferred<ASProjectile>(ProjectileClass, FTransform(MuzzleRotation, MuzzleLocation), this, UGameplayStatics::GetPlayerPawn(GetWorld(), 0), ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding);
+    ASProjectile* projectile = GetWorld()->SpawnActorDeferred<ASProjectile>(ProjectileClass, FTransform(EyeRotation, MuzzleLocation), this, UGameplayStatics::GetPlayerPawn(GetWorld(), 0), ESpawnActorCollisionHandlingMethod::AdjustIfPossibleButDontSpawnIfColliding);
 
     if(projectile)
     {
