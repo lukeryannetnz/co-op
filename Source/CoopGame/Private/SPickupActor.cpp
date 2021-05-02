d651b57070779294a622d9eb0855f679e6a54171
Luke Ryan <github@lukeryan.net.nz>
2021-05-02 19:06:53 +1200

    Add class for pickup actor.

---
+++ Source/CoopGame/Private/SPickupActor.cpp
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SPickupActor.h"
+
+// Sets default values
+ASPickupActor::ASPickupActor()
+{
+
+}
+
+// Called when the game starts or when spawned
+void ASPickupActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
